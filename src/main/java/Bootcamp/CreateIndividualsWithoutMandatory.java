594e73cd357e67cf8b004402fc8a0fc6d0a8f1ca
kanag <kanag@LAPTOP-QDS9V70J>
2021-05-30 09:14:54 +0530

    Create individual & delete code

--- src/main/java/Bootcamp/CreateIndividualsWithoutMandatory.java
+++ src/main/java/Bootcamp/CreateIndividualsWithoutMandatory.java
@@ -1,5 +1,6 @@
 package Bootcamp;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
@@ -12,7 +13,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class DeleteIndividual {
+public class CreateIndividualsWithoutMandatory {
 
 	public static void main(String[] args) throws InterruptedException {
 		ChromeOptions options = new ChromeOptions();
@@ -24,36 +25,35 @@ public class DeleteIndividual {
 		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
 		driver.findElement(By.xpath("//input[@id='username']")).sendKeys("cypress@testleaf.com");
 		driver.findElement(By.xpath("//input[@id='password']")).sendKeys("Selbootcamp@123");
-		driver.findElement(By.xpath("//input[@type='submit']")).click();
+		driver.findElement(By.xpath("//input[@type='submit']")).click();	
 		driver.findElement(By.xpath("//div[@class='slds-icon-waffle']")).click();
-		WebDriverWait wait = new WebDriverWait(driver, 30);
 		WebElement ViewAllbutton = driver.findElement(By.xpath("(//button[text()='View All'])[2]"));
-		wait.until(ExpectedConditions.elementToBeClickable(ViewAllbutton));
 		ViewAllbutton.click();
 		WebElement Individuals = driver.findElementByXPath("//p[text()='Individuals']");
 		driver.executeScript("arguments[0].scrollIntoView();", Individuals);
 		Individuals.click();
-		Thread.sleep(2000);
-		WebElement IndividualTab = driver.findElementByXPath("(//span[contains(text(),'Individuals')])[2]");
-		driver.executeScript("arguments[0].click;", IndividualTab);
-		Thread.sleep(2000);
-		driver.findElementByXPath("//input[@placeholder='Search this list...']").sendKeys("kumar");
-		Thread.sleep(2000);
-		driver.findElementByXPath("//button[@name='refreshButton']").click();
-		Thread.sleep(2000);
-		WebElement last = driver.findElementByXPath("//span[text()='Last Modified Date']");
-		driver.executeScript("arguments[0].click();", last);
-		Thread.sleep(2000);
-		driver.findElementByXPath("//a[contains(@class,'rowActionsPlaceHolder')]/span/span").click();
-		driver.findElementByXPath("//a[@title='Delete']").click();
-		driver.findElementByXPath("//span[text()='Delete']").click();
-		WebElement message = driver.findElementByXPath("//span[contains(@class,'toastMessage')]");
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[contains(@class,'toastMessage')]")));
-		String output = message.getText();
-		String[] split = output.split(" ");
-		System.out.println("The individual "+split[1]+ " is deleted successfully");
+		Thread.sleep(3000);
+		WebElement drop = driver.findElementByXPath("(//lightning-icon[@class='slds-icon-utility-chevrondown slds-icon_container']/lightning-primitive-icon/*)[15]");
+		//driver.executeScript("arguments[0].click();", drop);
+		drop.click();
+		Thread.sleep(3000);
+		WebElement newIndividual = driver.findElementByXPath("//span[text()='New Individual']");
+		driver.executeScript("arguments[0].click();", newIndividual);
+		driver.findElementByXPath("//a[@class='select']").click();
+		List<WebElement> salutationList = driver.findElementsByXPath("//li[@role='presentation']");
+		int size = salutationList.size();
+		for (int i = 0; i < size; i++) {
+			String text = salutationList.get(i).getText();
+			if(text.contains("Mr.")) {
+				salutationList.get(i).click();
+				
+			}
+		}
+		driver.findElementByXPath("//input[contains(@class,'firstName')]").sendKeys("Ganesh");
+		driver.findElementByXPath("//*[@title='Save']").click();
+		String text = driver.findElementByXPath("//input[contains(@class,'lastName')]/following::ul/li").getText();
+		System.out.println("Error message received: "+text);
 		
-
 	}
 
 }
